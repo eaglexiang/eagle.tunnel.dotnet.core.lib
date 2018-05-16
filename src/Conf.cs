76ade651109ded64003d2d48039d0696dff057bd
eaglexiang <eagle.xiang@outlook.com>
2018-05-16 21:38:50 +0800

    lists must be imported

--- src/Conf.cs
+++ src/Conf.cs
@@ -285,15 +285,13 @@ namespace eagle.tunnel.dotnet.core {
             if (allConf.ContainsKey ("proxy status")) {
                 proxyStatus = (ProxyStatus) Enum.Parse (typeof (Conf.ProxyStatus),
                     allConf["proxy status"][0].ToUpper ());
-                if (proxyStatus == ProxyStatus.SMART) {
-                    ImportList ("whitelist_domain.txt", out whitelist_domain);
-                    ImportList ("whitelist_ip.txt", out whitelist_ip);
-                    ImportList ("blacklist_ip.txt", out blacklist_ip);
-                }
             } else {
                 proxyStatus = ProxyStatus.ENABLE; // default enable proxy
             }
             Console.WriteLine ("Proxy Status: {0}", proxyStatus.ToString ());
+            ImportList ("whitelist_domain.txt", out whitelist_domain);
+            ImportList ("whitelist_ip.txt", out whitelist_ip);
+            ImportList ("blacklist_ip.txt", out blacklist_ip);
         }
 
         private static void ImportWhiteList () {
