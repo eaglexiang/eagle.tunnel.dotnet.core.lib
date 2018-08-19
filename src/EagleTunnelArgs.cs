4c8b5242c281115f807bf0e503ad33f15374a0ac
Eagle <eagle.xiang@outlook.com>
2018-08-20 04:09:30 +0800

    fix smart dns

--- src/EagleTunnelArgs.cs
+++ src/EagleTunnelArgs.cs
@@ -3,22 +3,28 @@ using System.Net;
 using System.Text;
 using System.Threading;
 
-namespace eagle.tunnel.dotnet.core {
-    public class EagleTunnelArgs {
+namespace eagle.tunnel.dotnet.core
+{
+    public class EagleTunnelArgs
+    {
         public EagleTunnelUser User { get; set; }
         private string domain;
-        public string Domain {
-            get {
+        public string Domain
+        {
+            get
+            {
                 return domain;
             }
-            set {
+            set
+            {
                 domain = value;
-                switch (Conf.Status) {
+                switch (Conf.Status)
+                {
                     case Conf.ProxyStatus.DISABLE:
                         EnableProxy = false;
                         break;
                     case Conf.ProxyStatus.SMART:
-                        EnableProxy = CheckEnableProxy (domain);
+                        EnableProxy = CheckEnableProxy(domain);
                         break;
                     default:
                         EnableProxy = true;
@@ -30,19 +36,24 @@ namespace eagle.tunnel.dotnet.core {
         public bool Success { get; set; }
         public bool EnableProxy { get; set; }
         private IPEndPoint endPoint;
-        public IPEndPoint EndPoint {
-            get {
+        public IPEndPoint EndPoint
+        {
+            get
+            {
                 return endPoint;
             }
-            set {
+            set
+            {
                 endPoint = value;
-                if (endPoint != null) {
-                    switch (Conf.Status) {
+                if (endPoint != null)
+                {
+                    switch (Conf.Status)
+                    {
                         case Conf.ProxyStatus.DISABLE:
                             EnableProxy = false;
                             break;
                         case Conf.ProxyStatus.SMART:
-                            EnableProxy = CheckEnableProxy (endPoint.Address);
+                            EnableProxy = CheckEnableProxy(endPoint.Address);
                             break;
                         default:
                             EnableProxy = true;
@@ -52,35 +63,47 @@ namespace eagle.tunnel.dotnet.core {
             }
         }
 
-        public EagleTunnelArgs () {
+        public EagleTunnelArgs()
+        {
             Success = false;
         }
 
-        private static bool CheckEnableProxy (string domain) {
+        private static bool CheckEnableProxy(string domain)
+        {
             bool result = false;
-            if (!string.IsNullOrEmpty (domain)) {
-                foreach (string item in Conf.whitelist_domain) {
-                    if (domain.IndexOf (item) >= 0) {
-                        result = true;
-                        break;
+            if (!string.IsNullOrEmpty(domain))
+            {
+                foreach (string item in Conf.whitelist_domain)
+                {
+                    int ind = domain.LastIndexOf(item);
+                    if(ind>=0){
+                        if((ind+item.Length)==domain.Length){
+                            result=true;
+                            break;
+                        }
                     }
                 }
             }
             return result;
         }
 
-        private static bool CheckEnableProxy (IPAddress ip) {
+        private static bool CheckEnableProxy(IPAddress ip)
+        {
             bool result = true;
-            if (ip != null) {
-                EagleTunnelArgs e = new EagleTunnelArgs ();
+            if (ip != null)
+            {
+                EagleTunnelArgs e = new EagleTunnelArgs();
                 e.IP = ip;
-                EagleTunnelSender.Handle (
+                EagleTunnelSender.Handle(
                     EagleTunnelHandler.EagleTunnelRequestType.LOCATION,
                     e
                 );
-                if (e.Success) {
+                if (e.Success)
+                {
                     result = e.EnableProxy;
-                } else {
+                }
+                else
+                {
                     result = true;
                 }
             }
